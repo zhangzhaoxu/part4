21af53ca2302b90e6541179f97385f59d0da0ae2
zhangzhaoxu <755760826@qq.com>
2015-11-20 12:10:50 +0800

    second change

--- js/controller.js
+++ js/controller.js
@@ -5,14 +5,14 @@ angular.module('App').controller('nutritionController', ['$http', '$q', '$timeou
     $scope.showEdit = true;
 
 
-    $scope.deleteSelected = function (selects) {
-        console.log(selects);
-        selects.forEach(function (select) {
-            $scope.desserts.data.splice($scope.desserts.data.indexOf(select),1);
-            $scope.desserts.count--;
-        });
-        $scope.selected=[];
-    };
+    //$scope.deleteSelected = function (selects) {
+    //    console.log(selects);
+    //    selects.forEach(function (select) {
+    //        $scope.desserts.data.splice($scope.desserts.data.indexOf(select),1);
+    //        $scope.desserts.count--;
+    //    });
+    //    $scope.selected=[];
+    //};
 
     $scope.$watch('selected', function (newValue, oldValue, scope) {
         if(newValue.length>1){
@@ -20,35 +20,34 @@ angular.module('App').controller('nutritionController', ['$http', '$q', '$timeou
         }
     },true);
 
-    $scope.showConfirm = function(ev,selects) {    //删除确认
-        var confirm = $mdDialog.confirm()
-            .title('你确定要删除吗?')
-            .content('删除后无法恢复哦！')
-            .targetEvent(ev)
-            .ok('删除')
-            .cancel('取消');
-        $mdDialog.show(confirm).then(function() {
-            $scope.deleteSelected(selects); //删除
-        }, function() {
-            //取消删除
-        });
-    };
+    //$scope.showConfirm = function(ev,selects) {    //删除确认
+    //    var confirm = $mdDialog.confirm()
+    //        .title('你确定要删除吗?')
+    //        .content('删除后无法恢复哦！')
+    //        .targetEvent(ev)
+    //        .ok('删除')
+    //        .cancel('取消');
+    //    $mdDialog.show(confirm).then(function() {
+    //        $scope.deleteSelected(selects); //删除
+    //    }, function() {
+    //        //取消删除
+    //    });
+    //};
+    //
+    //$scope.Edit = function (dessert) {
+    //    var index = $scope.desserts.data.indexOf(dessert);
+    //    $scope.desserts.data[index].ReadOrWrite = !$scope.desserts.data[index].ReadOrWrite;
+    //    $scope.desserts.data[index].showEdit = !$scope.desserts.data[index].showEdit;
+    //    console.log($scope.showEdit);
+    //};
 
-    $scope.Edit = function (dessert) {
-        console.log(dessert);
-        var index = $scope.desserts.data.indexOf(dessert);
-        $scope.desserts.data[index].ReadOrWrite = !$scope.desserts.data[index].ReadOrWrite;
-        $scope.desserts.data[index].showEdit = !$scope.desserts.data[index].showEdit;
-        console.log($scope.showEdit);
-    };
-
-    $scope.Save = function (dessert) {
-        console.log(dessert);
-        //此处添加向后台修改的申请，成功推出编辑模式，失败提示
-        var index = $scope.desserts.data.indexOf(dessert);
-        $scope.desserts.data[index].ReadOrWrite = !$scope.desserts.data[index].ReadOrWrite;
-        $scope.desserts.data[index].showEdit = !$scope.desserts.data[index].showEdit;
-    };
+    //$scope.Save = function (dessert) {
+    //    console.log(dessert);
+    //    //此处添加向后台修改的申请，成功推出编辑模式，失败提示
+    //    var index = $scope.desserts.data.indexOf(dessert);
+    //    $scope.desserts.data[index].ReadOrWrite = !$scope.desserts.data[index].ReadOrWrite;
+    //    $scope.desserts.data[index].showEdit = !$scope.desserts.data[index].showEdit;
+    //};
 
     $scope.saveRemark = function (selected) {
         $scope.selected.splice(0,1);
